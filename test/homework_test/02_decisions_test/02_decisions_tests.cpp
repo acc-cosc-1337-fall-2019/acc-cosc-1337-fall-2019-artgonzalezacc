374790ac946ba3bf6b454c8298321a97f4b4618c
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-09-13 18:26:05 -0500

    Homework 1 solution

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,9 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "decisions.h"
+#include "../../../homework/02_decisions/decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 	
 }
 
+TEST_CASE("Test get grade points") 
+{
+	REQUIRE(get_grade_points("A") == 4);
+}
