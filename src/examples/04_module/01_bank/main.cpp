253c664279ae693412a186986a15d311db58662b
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-07 19:00:12 -0500

    Class composition example

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 #include<vector>
-#include "bank_account.h"
+#include "atm.h"
 
-using std::cout;
-using std::vector;
+using std::cout; using std::vector;
 
 int main()
 {
-	vector<BankAccount> accounts;
-	BankAccount b1;
-	
-	accounts.push_back(b1);
-	accounts.push_back(BankAccount(750));
-	
-	for (auto account : accounts)
-	{
-		cout << "Balance: " << account.get_balance() <<"\n";
-	}
+	BankAccount a;
+	BankAccount b = a;
+
+	//scan card and enter pin
+	//get customer records from db into a vector
+	vector<BankAccount> accounts{BankAccount(500), BankAccount(600),
+	BankAccount(1000)};
+
+	ATM atm(accounts[0]);
+	atm.deposit(100);
+	atm.withdraw(50);
+	atm.display_balance();
+	//cout<<atm;
 
 	return 0;
 }
