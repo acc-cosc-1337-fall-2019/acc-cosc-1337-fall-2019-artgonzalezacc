67ffc7c13309459a8c7753609ed833978eceb280
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-14 18:13:22 -0500

    Class assignment 5 REctangle

--- src/classwork/05_assign/rectangle.cpp
+++ src/classwork/05_assign/rectangle.cpp
@@ -15,3 +15,10 @@ int acc::Rectangle::get_area()const
 {
 	return area;
 }
+
+std::ostream & acc::operator<<(std::ostream & out, const Rectangle & r)
+{
+	out << "Area: " << r.area <<'\n';
+
+	return out;
+}
