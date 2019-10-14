1343a150e0ba317947a4fcf52d9367837a8d39e3
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-14 18:22:15 -0500

    Customer vector of accounts example

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,30 +1,18 @@
 #include<iostream>
 #include<vector>
 #include "atm.h"
+#include "customer.h"
 
 using std::cout; using std::vector; using std::cin;
 
 int main()
 {
-	BankAccount a;
-	BankAccount b = a;
-	display(a);//class friend function
-	//scan card and enter pin
-	//get customer records from db into a vector
-	vector<BankAccount> accounts{BankAccount(500), BankAccount(600),
-	BankAccount(1000)};
-
-	for (auto act : accounts) 
-	{
-		cout << act;
-	}
-
-	ATM atm(accounts[0]);
-	atm.deposit(100);
-	atm.withdraw(50);
-	atm.display_balance();
-	cin >> a;
-	cout << a;// overload operator
+	Customer customer;
+	customer.add_account(BankAccount(500));
+	customer.add_account(BankAccount(1500));
+	customer.add_account(BankAccount(5000));
+	
+	cout << customer;
 
 	return 0;
 }
