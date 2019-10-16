879f242f54ee4db395edcc718568fff376785708
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-16 18:51:54 -0500

    Inheritance, overload +, override get balance

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,14 +1,24 @@
 #include<iostream>
 #include<vector>
 #include"checking_account.h"
+#include"savings_account.h"
 
 using std::cout; using std::vector; using std::cin;
 
 int main()
 {	
 	CheckingAccount a(1500);
-	cout << a.get_balance();
-	
+	cout << a;
+
+	SavingsAccount savings(500);
+	cout<<"Calls bank account overload ostream" << savings;
+	cout<<"\n Savings get_balance" << savings.get_balance()<<"\n";
+	savings.add_interest();
+	cout << savings;
+
+	BankAccount c = a + savings;
+
+	cout << c;
 
 	return 0;
 }
