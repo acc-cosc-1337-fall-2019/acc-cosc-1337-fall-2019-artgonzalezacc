8dad87de9f6079aee745bb436d9fb4772c7d602f
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-19 13:58:22 -0500

    HW 5 solution

--- src/homework/04_vectors/main.cpp
+++ src/homework/04_vectors/main.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include "vectors.h"
+
+using std::cout; using std::cin;
+
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -9,6 +14,44 @@ Program continues until user decides to exit.
 */
 int main() 
 {
+	vector<int> nums{ 8, 4, 20, 88, 66, 99 };
+	char choice = 'y';
+	int user_choice;
+
+	do 
+	{
+		cout << "Enter 1 for get max, 2 for get primes: ";
+		cin >> user_choice;
+
+		if(user_choice == 1)
+		{
+			cout << "Max in vector with values 8, 4, 20, 88, 66, 99: " << get_max_from_vector(nums) << "\n";
+		}
+		else if(user_choice == 2)
+		{
+			int num;
+			cout << "Enter a number: ";
+			cin >> num;
+			cout << "Primes of " << num << "\n";
+			vector<int> result = vector_of_primes(num);
+
+			for(auto n: result)
+			{
+				cout << n << " ";
+			}
+
+			cout << "\n";
+
+		}
+		else 
+		{
+			cout << "incorrect choice \n";
+		}
+
+		cout << "continue enter y";
+		cin >> choice;
+
+	} while (choice == 'y' || choice == 'Y');
 
 	return 0;
 }
--- src/homework/04_vectors/vectors.cpp
+++ src/homework/04_vectors/vectors.cpp
@@ -1,3 +1,5 @@
+
+#include "vectors.h"
 /*
 Write a value return function get_max_from_vector with a const reference 
 vector of intsparameter that returns the max value in a vector
@@ -5,16 +7,46 @@ vector of intsparameter that returns the max value in a vector
 @param numbers is a const reference to a vector of integers
 @return the max value in the vector
 */
+int get_max_from_vector(vector<int>& nums) 
+{
+	int max = nums[0];//first vector int is the max
+	
+	for (auto num : nums)
+	{
+		if (num > max) 
+		{
+			max = num;
+		}
+	}
 
+	return max;
+}
 
 
 /*
 Write a function named is_prime with an integer parameter that
 given a number returns true if prime or false if not prime
-
+Use brute method to determine if number divisible by 2
 @param number: Any whole number
 @return: bool if prime False if not
 */
+bool is_prime(int num)
+{
+	if (num <= 1) 
+	{
+		return false;
+	}
+
+	for (int i = 2; i < num; i++) 
+	{
+		if (num % i == 0) 
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
 
 /*
 Write a a function named vector_of_primes with an integer parameter
@@ -28,3 +60,17 @@ Example given number 10 returns a vector with elements 2,3,5,7,
 Make sure to use the is_prime function to determine if current 
 number is prime.
 */
+vector<int> vector_of_primes(int num)
+{
+	vector<int> primes;
+
+	for(int i=0; i < num;i++)
+	{
+		if (is_prime(i)) 
+		{
+			primes.push_back(i);
+		}
+	}
+
+	return primes;
+}
