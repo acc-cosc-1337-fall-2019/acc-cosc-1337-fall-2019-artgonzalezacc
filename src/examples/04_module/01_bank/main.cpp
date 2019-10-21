9c617598ff013763de5d632183491c2e5cf2f0a8
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-21 18:10:12 -0500

    Inheritance polymorphic reference

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -7,18 +7,14 @@ using std::cout; using std::vector; using std::cin;
 
 int main()
 {	
-	CheckingAccount a(1500);
-	cout << a;
+	CheckingAccount checking(1500);
+	cout << "\n Checking get_balance: " << checking.get_balance() << "\n";
 
 	SavingsAccount savings(500);
-	cout<<"Calls bank account overload ostream" << savings;
-	cout<<"\n Savings get_balance" << savings.get_balance()<<"\n";
-	savings.add_interest();
-	cout << savings;
+	cout<<"\n Savings get_balance: " << savings.get_balance()<<"\n";
 
-	BankAccount c = a + savings;
-
-	cout << c;
+	BankAccount& account = savings;
+	cout << "\n Ref to savings get_balance: " << account.get_balance()<<"\n";
 
 	return 0;
 }
