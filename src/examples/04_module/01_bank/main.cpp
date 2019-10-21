dcf18a6ec9dd5923ae76c5dae0e1af51c18ed6bb
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-21 18:22:38 -0500

    vector ref wrapper objects

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,20 +1,23 @@
+#include<functional>
 #include<iostream>
 #include<vector>
 #include"checking_account.h"
 #include"savings_account.h"
 
 using std::cout; using std::vector; using std::cin;
+using std::reference_wrapper;
 
 int main()
 {	
-	CheckingAccount checking(1500);
-	cout << "\n Checking get_balance: " << checking.get_balance() << "\n";
+	CheckingAccount c(1500) ;
+	SavingsAccount s(500) ;
 
-	SavingsAccount savings(500);
-	cout<<"\n Savings get_balance: " << savings.get_balance()<<"\n";
+	vector<reference_wrapper<BankAccount>> accounts{ c , s };
 
-	BankAccount& account = savings;
-	cout << "\n Ref to savings get_balance: " << account.get_balance()<<"\n";
+	for (auto act : accounts) 
+	{
+		cout << "Balance: " << act.get().get_balance() <<"\n";
+	}
 
 	return 0;
 }
