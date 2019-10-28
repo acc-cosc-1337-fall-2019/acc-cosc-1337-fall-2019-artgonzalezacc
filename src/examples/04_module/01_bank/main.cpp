65f764670ed0b50093055518194d2cbbac8cb1f0
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-28 18:23:58 -0500

    Dynamic memory example

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -4,23 +4,10 @@ using std::cout;
 
 int main()
 {	
-	int num = 5;
-	int & num_ref = num; //get the address of num
-	std::cout << "Address: " << &num_ref << std::endl; //output address
-	std::cout << "Value at above address: " << num_ref << std::endl; //output value at 
-
-	//point to the address of num
-	int * val_ = &num_ref; 
-	//get the address of num
-	std::cout << "Address pointed to: " << val_ << std::endl;
-	//get the value pointed to by val_
-	std::cout << "Value pointed to: " << *val_ << std::endl; 
-
-	*val_ = 15;
-	//get the address of num
-	std::cout << "Address pointed to: " << val_ << std::endl;
-	//get the value pointed to by val_
-	std::cout << "Value pointed to: " << *val_ << std::endl;
+	int * ptr_num = new int(5); //acquire dynamic memory
+	std::cout << "Address of: " << ptr_num << std::endl;
+	std::cout << "Value pointed to: " << *ptr_num;
+	delete ptr_num; //YOU MUST REMEMBER TO DELETE ALLOCATED MEMORY
 
 	return 0;
 }
