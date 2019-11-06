f3e5ea57d7a5009f176359b9c76a2f82129cec12
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-11-05 20:54:31 -0600

    Assignment 9

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,4 +1,6 @@
 #include "tic_tac_toe_manager.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 #include<iostream>
 #include<string>
 
@@ -9,23 +11,35 @@ int main()
 	char cont = 'y';
 	TicTacToeManager manager;
 	string player;
+	int game_type;
 
 	do 
 	{
-		TicTacToe game;
+		cout << "Play win by 3 or 4: ";
+		cin >> game_type;
+		TicTacToe* game;
+
+		if (game_type == 3) 
+		{
+			game = new TicTacToe3();
+		}
+		else 
+		{
+			game = new TicTacToe4();
+		}
 
 		cout << "Enter X or O";
 		cin >> player;
+		
+		game->start_game(player);
 
-		game.start_game(player);
-
-		while(game.game_over() == false)
+		while(game->game_over() == false)
 		{
-			cin >> game;
-			cout<<game;
+			cin >> *game;
+			cout<< *game;
 		}
 
-		manager.save_game(game);
+		manager.save_game(*game);
 	
 		cout << "Game over: ";
 
--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,21 +1,22 @@
 //h
 #ifndef TIC_TAC_TOE_MANAGER_H
 #define TIC_TAC_TOE_MANAGER_H
+#include<functional>
 #include<vector>
 #include "tic_tac_toe.h"
 
 using std::vector;
-
+using std::reference_wrapper;
 
 class TicTacToeManager 
 {
 public:
-	void save_game(const TicTacToe game);
+	void save_game(TicTacToe& game);
 	friend ostream& operator<<(ostream& out, const TicTacToeManager& m);
 
 private:
 	void update_winner_count(string winner);
-	vector<TicTacToe> games;
+	vector<reference_wrapper<TicTacToe>> games;
 	int x_win{ 0 };
 	int o_win{ 0 };
 	int ties{ 0 };
