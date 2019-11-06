a3eff614c6e8285f68b621f9d2eb869a515c576b
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-11-05 21:10:02 -0600

    HW 10 solution

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
+using std::cin;
+
+int main() 
+{
+	//TicTacToe3 three;
+	std::unique_ptr<TicTacToeManager> manager = std::make_unique<TicTacToeManager>();
+	auto option = 'y';
+	int choice = 0;
+	int dim;
+	unique_ptr<TicTacToe> board;
+	do 
+	{
+		cout << "Would you like to play with dimensions of 3 or 4?";
+		cin >> dim;
+		
+		if (dim == 3)
+		{
+			board = std::make_unique<TicTacToe3>();
+		}
+		else
+		{
+			board = std::make_unique<TicTacToe4>();
+		}
+		board->start_game("X");
+		while (board->game_over() == false)
+		{
+			cin >> *board;
+			cout << *board;
+
+		}
+
+		manager->save_game(board);
+
+		cout << "Enter y to play again. \n";
+		cin >> option;
+	} while (option == 'y' || option == 'Y');
+
+	cout << "History: \n";
+	cout << *manager;
+	
+	return 0;
+}
---
+++ tic_tac_toe_manager.h
@@ -0,0 +1,27 @@
+//h
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+#include "tic_tac_toe.h"
+#include<vector>
+#include<iostream>
+#include<memory>
+
+using std::unique_ptr;
+using std::ostream;
+using std::vector;
+
+class TicTacToeManager
+{
+public:
+	void save_game(unique_ptr<TicTacToe>& game);
+	friend ostream& operator<<(ostream& out, const TicTacToeManager& m);
+private:
+	void update_winner_count(string winner);
+	vector<unique_ptr<TicTacToe>> games;
+	int x_win{ 0 };
+	int o_win{ 0 };
+	int ties{ 0 };
+};
+
+#endif // !TIC_TAC_TOE_MANAGER_H
